a482d4f0167488a9178d7c3037da67cfd29d40f9
Bo Jordan <bo@bojordan.com>
2012-05-12 18:00:19 -0400

    initial add

---
+++ RegexTest/RegexTest/RegexTestForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace RegexTest
+{
+    partial class RegexTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.patternTextBox = new System.Windows.Forms.TextBox();
+            this.stringTextBox = new System.Windows.Forms.RichTextBox();
+            this.resultsTextBox = new System.Windows.Forms.RichTextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // patternTextBox
+            // 
+            this.patternTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.patternTextBox.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.patternTextBox.Location = new System.Drawing.Point(13, 12);
+            this.patternTextBox.Name = "patternTextBox";
+            this.patternTextBox.Size = new System.Drawing.Size(417, 25);
+            this.patternTextBox.TabIndex = 1;
+            this.patternTextBox.TextChanged += new System.EventHandler(this.EvaluateMatches);
+            // 
+            // stringTextBox
+            // 
+            this.stringTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.stringTextBox.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stringTextBox.Location = new System.Drawing.Point(0, 0);
+            this.stringTextBox.Name = "stringTextBox";
+            this.stringTextBox.Size = new System.Drawing.Size(417, 316);
+            this.stringTextBox.TabIndex = 2;
+            this.stringTextBox.Text = "";
+            this.stringTextBox.TextChanged += new System.EventHandler(this.EvaluateMatches);
+            // 
+            // resultsTextBox
+            // 
+            this.resultsTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resultsTextBox.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultsTextBox.Location = new System.Drawing.Point(0, 0);
+            this.resultsTextBox.Name = "resultsTextBox";
+            this.resultsTextBox.ReadOnly = true;
+            this.resultsTextBox.Size = new System.Drawing.Size(417, 312);
+            this.resultsTextBox.TabIndex = 3;
+            this.resultsTextBox.Text = "";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(13, 43);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.stringTextBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.resultsTextBox);
+            this.splitContainer1.Size = new System.Drawing.Size(417, 632);
+            this.splitContainer1.SplitterDistance = 316;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // RegexTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 687);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.patternTextBox);
+            this.Name = "RegexTestForm";
+            this.Text = "RegexTest";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox patternTextBox;
+        private System.Windows.Forms.RichTextBox stringTextBox;
+        private System.Windows.Forms.RichTextBox resultsTextBox;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ RegexTest/RegexTest/RegexTestForm.cs
@@ -0,0 +1,38 @@
+﻿
+namespace RegexTest
+{
+    using System;
+    using System.Text;
+    using System.Windows.Forms;
+    using System.Text.RegularExpressions;
+
+    public partial class RegexTestForm : Form
+    {
+        public RegexTestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EvaluateMatches(object sender, EventArgs args)
+        {
+            try
+            {
+                var matches = Regex.Matches(this.stringTextBox.Text, this.patternTextBox.Text);
+
+                this.resultsTextBox.Text = string.Empty;
+                string matchOutput = "{0} @{1}:{2} {3}";
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    sb.AppendLine(string.Format(matchOutput, i, matches[i].Index, matches[i].Length, matches[i]));
+                }
+
+                this.resultsTextBox.Text = sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                this.resultsTextBox.Text = ex.Message;
+            }
+        }
+    }
+}
